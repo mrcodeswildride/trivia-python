807a212768013d6352731f2b4f19ca29cda26a14
David Keller <kellerdb@gmail.com>
2021-12-09 09:35:26 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# trivia-python
+
+Trivia app written in Python.
+
+Use this app at [trinket.io](https://trinket.io/embed/python3/57c68e7f0b?outputOnly=true&start=result).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ main.py
@@ -0,0 +1,22 @@
+print()
+
+answer = input("What is the largest ocean in the world? ")
+
+if answer.lower() == "pacific":
+  print("Correct!")
+else:
+  print("Wrong. The correct answer is Pacific.")
+
+answer = input("\nWhich country has the biggest population? ")
+
+if answer.lower() == "china":
+  print("Correct!")
+else:
+  print("Wrong. The correct answer is China.")
+
+answer = input("\nHow many planets are in the solar system? ")
+
+if answer == "8":
+  print("Correct!")
+else:
+  print("Wrong. The correct answer is 8.")
